682f05274de5456e9df3e3c534cbff3331c4b5bb
EH STORE <98456625+EH4404@users.noreply.github.com>
2022-10-15 10:13:12 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+os.system('clear')
+print('')
+loga = ("""⠀⠀⠀⠀⠀⠀═══════════════════════════════════════════
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\033[1;31m╔═══╦╗⠀╔╗⠀⠀╔════╦═══╦═══╦╗\033[1;93m
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╔══╣║⠀║║⠀⠀║╔╗╔╗║╔═╗║╔═╗║║\033[0m
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╚══╣╚═╝║⠀⠀╚╝║║╚╣║⠀║║║⠀║║║\033[1;96m
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╔══╣╔═╗║⠀⠀⠀⠀║║⠀║║⠀║║║⠀║║║⠀╔╗\033[0;36m
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╚══╣║⠀║║⠀⠀⠀⠀║║⠀║╚═╝║╚═╝║╚═╝║\033[0;34m
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀╚═══╩╝⠀╚╝⠀⠀⠀⠀╚╝⠀╚═══╩═══╩═══╝\033[0;37m
+
+⠀⠀⠀⠀⠀⠀═══════════════════════════════════════════""""")
+print("")
+print(loga)
+print("")
+print("")
+print(" [\033[1;94mdiscord\033[1;0m] : https://discord.gg/kzAvy65e4R")
+print("")
+print(" [\033[1;91mYoutube\033[1;0m] : EH STORE")
+print("")
+enter = input("\033[92mEnter \033[1;0mเพื่อไปต่อ \033[1;93m> ")
+os.system("clear")
+#####os.system("pip install -U -r requirements.txt")
+os.system("clear")
+print("\033[1;0m")
+print(loga)
+print("")
+print(" [ \033[92mspamsms\033[0m ] : https://github.com/EH4404/ehstore")
+print("")
+print(" [ \033[96mfacebook\033[0m ] : https://www.facebook.com/EH.STORE999")
+print("")
