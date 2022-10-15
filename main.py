017f1375d1ea506cf6c25952d0e722aca2675f98
EH STORE <98456625+EH4404@users.noreply.github.com>
2022-10-15 10:15:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-import os
-os.system('clear')
-print('')
+import os #EHSTORE
+os.system('clear') #EHSTORE
+print('') #EHSTORE
 loga = ("""⠀⠀⠀⠀⠀⠀═══════════════════════════════════════════
 
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\033[1;31m╔═══╦╗⠀╔╗⠀⠀╔════╦═══╦═══╦╗\033[1;93m
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╔══╣║⠀║║⠀⠀║╔╗╔╗║╔═╗║╔═╗║║\033[0m
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╚══╣╚═╝║⠀⠀╚╝║║╚╣║⠀║║║⠀║║║\033[1;96m
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╔══╣║⠀║║⠀⠀║╔╗╔╗║╔═╗║╔═╗║║\033[0m #EHSTORE
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╚══╣╚═╝║⠀⠀╚╝║║╚╣║⠀║║║⠀║║║\033[1;96m #EHSTORE
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╔══╣╔═╗║⠀⠀⠀⠀║║⠀║║⠀║║║⠀║║║⠀╔╗\033[0;36m
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀║╚══╣║⠀║║⠀⠀⠀⠀║║⠀║╚═╝║╚═╝║╚═╝║\033[0;34m
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀╚═══╩╝⠀╚╝⠀⠀⠀⠀╚╝⠀╚═══╩═══╩═══╝\033[0;37m
 
-⠀⠀⠀⠀⠀⠀═══════════════════════════════════════════""""")
+⠀⠀⠀⠀⠀⠀═══════════════════════════════════════════""""") #EHSTORE
 print("")
 print(loga)
-print("")
+print("") #EHSTORE
 print("")
 print(" [\033[1;94mdiscord\033[1;0m] : https://discord.gg/kzAvy65e4R")
 print("")
 print(" [\033[1;91mYoutube\033[1;0m] : EH STORE")
 print("")
 enter = input("\033[92mEnter \033[1;0mเพื่อไปต่อ \033[1;93m> ")
-os.system("clear")
-#####os.system("pip install -U -r requirements.txt")
+os.system("clear") #EHSTORE
+os.system("pip install -U -r requirements.txt")
 os.system("clear")
 print("\033[1;0m")
-print(loga)
+print(loga) #EHSTORE
 print("")
 print(" [ \033[92mspamsms\033[0m ] : https://github.com/EH4404/ehstore")
-print("")
+print("") #EHSTORE
 print(" [ \033[96mfacebook\033[0m ] : https://www.facebook.com/EH.STORE999")
 print("")
+#EHSTORE
+#EH
